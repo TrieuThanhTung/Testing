bf199574e07bdd20fcfb052a2915fc409c06e63f
TrieuThanhTung <tung.ttrieu@gmail.com>
2023-09-27 12:51:13 +0700

    change function test

--- src/test/java/VoucherTest.java
+++ src/test/java/VoucherTest.java
@@ -6,61 +6,63 @@ public class VoucherTest {
 
     @Test
     void test1() {
-        assertEquals(new Voucher("food", -1000).verifyValue(), "invalid input");
+        assertEquals("invalid input", new Voucher("food", -1000).verifyValue());
     }
 
     @Test
     void test2() {
-        assertEquals(new Voucher("food", 150000).verifyValue(), Double.toString(0));
+        assertEquals(0, Double.parseDouble(new Voucher("food", 150000).verifyValue()));
     }
 
     @Test
     void test3() {
-        assertEquals(new Voucher("electron", -1500).verifyValue(), "invalid input");
+        assertEquals("invalid input", new Voucher("electron", -1500).verifyValue());
     }
 
     @Test
     void test4() {
-        assertEquals(new Voucher("electron", 300000).verifyValue(), Double.toString(0.05));
+        assertEquals(0.05, Double.parseDouble(new Voucher("electron", 300000).verifyValue()));
     }
 
     @Test
     void test5() {
-        assertEquals(new Voucher("electron", 700000).verifyValue(), Double.toString(0.1));
+        assertEquals(0.1, Double.parseDouble(new Voucher("electron", 700000).verifyValue()));
     }
 
     @Test
     void test6() {
-        assertEquals(new Voucher("electron", 3000000).verifyValue(), Double.toString(0.15));
+        assertEquals(0.15, Double.parseDouble(new Voucher("electron", 3000000).verifyValue()));
     }
 
     @Test
     void test7() {
         assertEquals(new Voucher("electron", 8000000).verifyValue(), Double.toString(0));
+        assertEquals(0, Double.parseDouble(new Voucher("electron", 8000000).verifyValue()));
     }
 
     @Test
     void test8() {
-        assertEquals(new Voucher("clothing", -1500).verifyValue(), "invalid input");
+        assertEquals("invalid input", new Voucher("clothing", -2000).verifyValue());
     }
 
     @Test
     void test9() {
-        assertEquals(new Voucher("clothing", 300000).verifyValue(), Double.toString(0));
+        assertEquals(0, Double.parseDouble(new Voucher("clothing", 400000).verifyValue()));
     }
+
 
     @Test
     void test10() {
-        assertEquals(new Voucher("clothing", 700000).verifyValue(), Double.toString(0.05));
+        assertEquals(0.05, Double.parseDouble(new Voucher("clothing", 900000).verifyValue()));
     }
 
     @Test
     void test11() {
-        assertEquals(new Voucher("clothing", 3000000).verifyValue(), Double.toString(0.1));
+        assertEquals(0.1, Double.parseDouble(new Voucher("clothing", 2000000).verifyValue()));
     }
 
     @Test
     void test12() {
-        assertEquals(new Voucher("clothing", 8000000).verifyValue(), Double.toString(0));
+        assertEquals(0, Double.parseDouble(new Voucher("clothing", 12000000).verifyValue()));
     }
 }
